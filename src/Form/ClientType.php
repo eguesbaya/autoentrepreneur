4335e415090d6bfc3b14136196ae7108f6ac2569
Emma Guesbaya <guesbaya@hotmail.fr>
2021-08-30 18:56:31 +0200

    radio buttons for client type

--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -6,15 +6,25 @@ use App\Entity\Client;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ClientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('type')
-            ->add('comment')
+            ->add('name', TextType::class)
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'organisation' => 'organisation',
+                    'individual' => 'individual',
+                    'other'=>'other'
+                ],
+                'multiple' => false,
+                'expanded' => true
+            ])
+            ->add('comment', TextType::class)
             ->add('photo')
         ;
     }
